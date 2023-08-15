baf705bb1edb0e38b46dcd465f26989c3d4b4321
turbo <joeyturbo@gmail.com>
2023-08-15 14:58:20 -0400

    added popular movie grid on home

--- js/script.js
+++ js/script.js
@@ -2,6 +2,54 @@ const global = {
   currentPage: window.location.pathname
 };
 
+async function displayPopularMovies() {
+  const {results} = await fetchAPIData('movie/popular');
+
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        ${
+          movie.poster_path 
+            ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+              />` 
+            : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+              />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#popular-movies').appendChild(div)
+  })
+}
+
+
+// fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '415996a786597887d8e8ca66275c019a';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+  const data = await response.json();
+
+  return data;
+
+}
+
 // highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -18,7 +66,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('shows');
